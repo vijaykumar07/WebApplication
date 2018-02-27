e1215170bfa183b67022680b4fab8e6bf8e149f6
vijaykumar07 <vijaykumar.it07@gmail.com>
2018-02-27 22:27:28 +0530

    Added about component and nav component

--- AngularApplication/src/app/app.module.ts
+++ AngularApplication/src/app/app.module.ts
@@ -4,28 +4,39 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router'
 
 // Import Components
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
+import { AboutComponent } from './components/about/about.component';
 
 //Import Services
 import { UserService } from './services/user.service';
 
 //Import Filters
 import { FilterPipe } from './pipes/filter.pipe';
+import { Route } from '@angular/compiler/src/core';
+import { Component } from '@angular/core/src/metadata/directives';
+
+const appRoutes: Routes = [
+  {path:'', component: UsersComponent},
+  {path:'about', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    FilterPipe
+    FilterPipe,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
--- AngularApplication/src/app/components/users/users.component.ts
+++ AngularApplication/src/app/components/users/users.component.ts
@@ -10,67 +10,67 @@ import { UserService } from '../../services/user.service'
 })
 export class UsersComponent implements OnInit {
 
-  users:User[];
-  userCopy:User[];
-  statusCode:number;
-  createUser:Object = {};
+  users: User[];
+  userCopy: User[];
+  statusCode: number;
+  createUser: any = {};
 
   //Create form
-   userForm = new FormGroup({
+  userForm = new FormGroup({
     "user-name": new FormControl('', Validators.required),
     "user-emailid": new FormControl('', Validators.required),
     "user-phone-number": new FormControl('', Validators.required),
     "user-company-name": new FormControl('', Validators.required),
-    "user-address": new FormControl('', Validators.required)  
-   });
+    "user-address": new FormControl('', Validators.required)
+  });
 
-   userIdToUpdate = null;
+  userIdToUpdate = null;
 
-  constructor(private userService:UserService) {
+  constructor(private userService: UserService) {
 
-   }
+  }
 
   ngOnInit() {
     this.loadAllUsers();
   }
 
-  loadAllUsers(){
+  loadAllUsers() {
     this.userService.getAllUsers().subscribe((users) => {
       this.users = users;
     });
   }
 
-  updateUser(user){
+  updateUser(user) {
     this.userCopy = user;
     this.userIdToUpdate = user.id;
-    this.userForm.setValue({ "user-name": user.name, "user-emailid": user.email, "user-phone-number":user.phone, "user-company-name":user.company.name, "user-address":user.address.street});
+    this.userForm.setValue({ "user-name": user.name, "user-emailid": user.email, "user-phone-number": user.phone, "user-company-name": user.company.name, "user-address": user.address.street });
   }
 
-  onUserFormSubmit(){
+  onUserFormSubmit() {
     //capture the form data
-    let userFormDetails:User = this.userForm.value;
+    let userFormDetails: User = this.userForm.value;
 
     let userName = userFormDetails['user-name'],
-        userEmailID = userFormDetails['user-emailid'],
-        userPhoneNumber = userFormDetails['user-phone-number'],
-        userCompanyName = userFormDetails['user-company-name'],
-        userAddress = userFormDetails['user-address'];
-    if(this.userIdToUpdate === null){
+      userEmailID = userFormDetails['user-emailid'],
+      userPhoneNumber = userFormDetails['user-phone-number'],
+      userCompanyName = userFormDetails['user-company-name'],
+      userAddress = userFormDetails['user-address'];
+    if (this.userIdToUpdate === null) {
       //Handle create 
       this.createUser['id'] = this.users.length + 1;
       this.createUser['name'] = userName;
       this.createUser['email'] = userEmailID;
       this.createUser['phone'] = userPhoneNumber;
-      this.createUser['company'] = {"name":userCompanyName};
-      this.createUser['address'] = {"street":userAddress};
+      this.createUser['company'] = { "name": userCompanyName };
+      this.createUser['address'] = { "street": userAddress };
 
       //Add User Details in UserModal
-      this.users.push(this.createUser); 
-      
-    }else{
+      this.users.push(this.createUser);
+
+    } else {
       //Handle update
       this.userService.getUserByID(this.userIdToUpdate).subscribe(user => {
-        
+
         user.name = userName;
         user.email = userEmailID;
         user.phone = userPhoneNumber;
@@ -79,55 +79,48 @@ export class UsersComponent implements OnInit {
 
         let self = this;
         this.users.forEach(function (value, index) {
-          if((self.userIdToUpdate-1) === index){
+          if ((self.userIdToUpdate - 1) === index) {
             //Update the users Model
             self.users[index] = user;
           }
         });
-
-        /*this.userService.updateUser(user).subscribe(successCode => {
-          this.statusCode = successCode;
-          console.log(this.statusCode);
-          this.loadAllUsers();
-          },
-         errorCode => this.statusCode = errorCode);
-          */
+        
       });
     }
 
   }
   // delete User from the user modal
-  deleteUser(index){
+  deleteUser(index) {
     let deleteUserIndex = index,
-        self = this;
+      self = this;
     this.users.forEach(function (user, index) {
-      if(index === deleteUserIndex){
+      if (index === deleteUserIndex) {
         self.users.splice(index, 1);
       }
-  });
+    });
 
+  }
 }
-}  
-interface User{
-  id:number,
-  name:string,
-  phone:number,
-  username:string,
-  website:string,
-  company:Company,
-  address:Address
-}
-
-interface Company{
-  bs:string,
-  catchPhrase:string,
-  name:string
+interface User {
+  id: number,
+  name: string,
+  phone: number,
+  username: string,
+  website: string,
+  company: Company,
+  address: Address
 }
 
-interface Address{
-  city:string,
-  geo:any,
-  street:string,
-  suite:string,
-  zipcode:number
+interface Company {
+  bs: string,
+  catchPhrase: string,
+  name: string
+}
+
+interface Address {
+  city: string,
+  geo: any,
+  street: string,
+  suite: string,
+  zipcode: number
 }
